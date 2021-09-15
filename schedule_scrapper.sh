e89160435cbaf73b0c407d762952340efc6810d8
Mariusz Pierzchała <mario.pierzchala@outlook.com>
2021-09-15 18:39:50 +0000

    Working bash script that find text on website

--- schedule_scrapper.sh
+++ schedule_scrapper.sh
@@ -2,4 +2,8 @@
 
 variable=$(curl --silent https://pwsztar.edu.pl/instytut-politechniczny/informatyka/harmonogramy/ | htmlq "#rozmCZ > p:nth-child(8)")
 
-echo $variable
+if [ "$variable" == "<p>Aktualizacja: 2021-05-24 13:40 CEST</p>" ]
+then
+  echo Hey: $variable
+fi
+
