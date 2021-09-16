3a04eb06e79fbb208f1b340f517023313ef32ba0
Mariusz Pierzchała <mario.pierzchala@outlook.com>
2021-09-16 22:39:38 +0200

    Test new schedule scrapper with python

--- schedule_scrapper.sh
+++ schedule_scrapper.sh
@@ -2,8 +2,10 @@
 
 variable=$(curl --silent https://pwsztar.edu.pl/instytut-politechniczny/informatyka/harmonogramy/ | htmlq "#rozmCZ > p:nth-child(8)")
 
-if [ "$variable" == "<p>Aktualizacja: 2021-05-24 13:40 CEST</p>" ]
-then
-  echo Hey: $variable
-fi
+# if [ "$variable" == "<p>Aktualizacja: 2021-05-24 13:40 CEST</p>" ]
+# then
+#   echo Hey: $variable
+# fi
+
+python scrapper.py $variable
 
