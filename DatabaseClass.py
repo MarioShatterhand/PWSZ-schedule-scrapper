4094e878afae99a4bbfd9a8157e008156ec729fc
Mariusz Pierzchała <mario.pierzchala@outlook.com>
2021-09-18 15:11:15 +0200

    Changes in DatabaseClass

--- DatabaseClass.py
+++ DatabaseClass.py
@@ -4,19 +4,15 @@ import sqlite3
 class DatabaseClass:
     def __init__(self) -> None:
         self.con = sqlite3.connect('dane.db')
-        print("Init innit?!")
 
     def fetchone(self, val: str):
         try:
-            print("Here it is")
-            print(val)
             cursor = self.con.cursor()
             cursor.execute("SELECT * FROM ostatnia_aktualizacja")
             self.con.commit()
-            print(cursor.fetchall())
-            result = cursor.fetchone()
+            # print(cursor.fetchall())
+            return cursor.fetchone()
            # cursor.close()
         except Exception as e:
             print(e)
-        return result
 
